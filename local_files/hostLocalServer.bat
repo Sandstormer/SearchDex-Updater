4b2142b90382249402f99cb62f7b63998912a3f8
Sandstormer <ert__@hotmail.com>
2025-12-05 21:45:27 -0500

    Fix Local Hosting

--- local_files/hostLocalServer.bat
+++ local_files/hostLocalServer.bat
@@ -11,10 +11,10 @@ echo Your local IP address is !IP!
 echo The website will be accessible at !IP!:8000
 echo .
 echo Use Firefox Mobile Debugging at about:debugging#/setup
+echo Enable USB Debugging in Android settings and Firefox settings
 echo .
 echo The local server will launch . . .
 pause
 echo .
 echo The local server is live
-cd website
-python -m http.server 8000 --bind 0.0.0.0
+python -m http.server 8000 --bind 0.0.0.0 --directory "%~dp0../website"
