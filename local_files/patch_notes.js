e090dae70bbf066e8aed7ed767d532bff76c6248
Sandstormer <ert__@hotmail.com>
2026-01-03 01:28:06 -0500

    Patch 1-11-6 Cost Changes

--- local_files/patch_notes.js
+++ local_files/patch_notes.js
@@ -8,6 +8,30 @@ Hariyama:<br>
 shvar: 1 > 3<br>
 newVariants:  > 1<br>
 <br>
+Normal Deoxys:<br>
+cost: 7 > 6<br>
+<br>
+Attack Deoxys:<br>
+cost: 7 > 6<br>
+<br>
+Defense Deoxys:<br>
+cost: 7 > 6<br>
+<br>
+Speed Deoxys:<br>
+cost: 7 > 6<br>
+<br>
+Alomomola:<br>
+cost: 4 > 3<br>
+<br>
+Terrakion:<br>
+cost: 6 > 7<br>
+<br>
+Incarnate Tornadus:<br>
+cost: 7 > 6<br>
+<br>
+Therian Tornadus:<br>
+cost: 7 > 6<br>
+<br>
 Comfey:<br>
 shvar: 1 > 3<br>
 newVariants:  > 1<br>
@@ -15,4 +39,19 @@ newVariants:  > 1<br>
 Dhelmise:<br>
 shvar: 1 > 3<br>
 newVariants:  > 1<br>
+<br>
+Poipole:<br>
+cost: 8 > 7<br>
+<br>
+Naganadel:<br>
+cost: 8 > 7<br>
+<br>
+Incarnate Enamorus:<br>
+cost: 7 > 6<br>
+<br>
+Therian Enamorus:<br>
+cost: 7 > 6<br>
+<br>
+Pecharunt:<br>
+cost: 6 > 5<br>
 `;
